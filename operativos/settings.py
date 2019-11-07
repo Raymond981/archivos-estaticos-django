2838bfaced4bd0a34b990ae7661188c68be75ae9
José Ramón Palacios Martínez <163163@ids.upchiapas.edu.mx>
2019-11-07 08:15:00 -0600

    Inicio

--- operativos/settings.py
+++ operativos/settings.py
@@ -131,17 +131,3 @@ USE_TZ = True
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'operativos/static'),
 ]
-
-AWS_ACCESS_KEY_ID = 'AKIA4FEM6CMWX5GQIC7U'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'django-static-bucket'
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-}
-AWS_LOCATION = 'static'
-
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-DEFAULT_FILE_STORAGE = 'operativos.storage_backeds.MediaStorage'
